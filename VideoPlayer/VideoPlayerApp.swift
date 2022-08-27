a6237aa5816bdce7ce27d09739cc853a597c4883
ebibibibibi <xvipea0@gmail.com>
2022-08-27 19:35:42 +0700

    Initial Commit

---
+++ VideoPlayer/VideoPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerApp.swift
+//  VideoPlayer
+//
+//  Created by kotomi tahkahashi on 2022/08/27.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
